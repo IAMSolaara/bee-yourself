474ce7551e9f1cec65f99ee1311a211e547f83e3
Sarah Evermore <sarah.evermore.02@gmail.com>
2024-07-01 10:29:10 +0200

    init :3

---
+++ src/main/java/net/solaara/beeyourself/BeeYourselfDataGenerator.java
@@ -0,0 +1,11 @@
+package net.solaara.beeyourself;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BeeYourselfDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
